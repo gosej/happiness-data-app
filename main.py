20e7bc175e873ad0fd0900db27c7e84a9319a4d2
jgose_macbook <jasongose@gmail.com>
2025-03-07 16:12:13 -0700

    Changed dropdown selection and some formatting for better presentation

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import plotly.express as px
 import pandas as pd
 
 
-# pick 3 variables to pull from csv and allow user to select what to plot on x/y axis
+# read csv
 df = pd.read_csv("happy.csv")
 
 st.title("Happiness Data Correlation Visualizer")
-x_option = st.selectbox("Select X-axis to plot: ", ("happiness","gdp","social_support","life_expectancy","freedom_to_make_life_choices","generosity","corruption"))
-y_option = st.selectbox("Select Y-axis to plot: ", ("happiness","gdp","social_support","life_expectancy","freedom_to_make_life_choices","generosity","corruption"))
+# x_option = st.selectbox("Select X-axis to plot: ", ("happiness","gdp","social_support","life_expectancy","freedom_to_make_life_choices","generosity","corruption"))
+# y_option = st.selectbox("Select Y-axis to plot: ", ("happiness","gdp","social_support","life_expectancy","freedom_to_make_life_choices","generosity","corruption"))
 
-x_data = df[x_option]
-y_data = df[y_option]
+x_option = st.selectbox("Select X-axis to plot: ", ("Happiness","GDP","Social Support","Life Expectancy","Freedom to Make Life Choices","Generosity","Corruption"))
+y_option = st.selectbox("Select Y-axis to plot: ", ("Happiness","GDP","Social Support","Life Expectancy","Freedom to Make Life Choices","Generosity","Corruption"))
+
+
+#modying the x/y_option format to match csv column names
+x_data = df[x_option.lower().replace(' ','_')]
+y_data = df[y_option.lower().replace(' ','_')]
 
 st.subheader(f"{x_option} and {y_option}")
 
