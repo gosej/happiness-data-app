9171100503ae9d1a6b9f72153e860ffd68e233e3
jgose_macbook <jasongose@gmail.com>
2025-03-07 16:00:39 -0700

    removed functions that overcomplicated simple data assignment

--- main.py
+++ main.py
@@ -10,29 +10,10 @@ st.title("Happiness Data Correlation Visualizer")
 x_option = st.selectbox("Select X-axis to plot: ", ("happiness","gdp","social_support","life_expectancy","freedom_to_make_life_choices","generosity","corruption"))
 y_option = st.selectbox("Select Y-axis to plot: ", ("happiness","gdp","social_support","life_expectancy","freedom_to_make_life_choices","generosity","corruption"))
 
-x_data = []
-y_data = []
-
-def build_x_dataset(x_option):
-    for index, row in df.iterrows():
-        x_data.append(row[x_option])
-    return x_data
-
-def build_y_dataset(y_option):
-    for index, row in df.iterrows():
-        y_data.append(row[y_option])
-    return y_data
-
-x_data = build_x_dataset(x_option)
-y_data = build_y_dataset(y_option)
-
-
-print(f"x_data = {x_data}")
-print(f"y_data = {y_data}")
-
+x_data = df[x_option]
+y_data = df[y_option]
 
 st.subheader(f"{x_option} and {y_option}")
-
 
 figure = px.scatter(x=x_data, y=y_data, labels={"x": x_option, "y": y_option})
 st.plotly_chart(figure)
