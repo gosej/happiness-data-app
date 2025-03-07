1bfef4cbaa8a1fec2654add2854c299f6e2c3104
jgose_macbook <jasongose@gmail.com>
2025-03-07 15:53:48 -0700

    created function to get data points from csv file

---
+++ README.md
@@ -0,0 +1,3 @@
+## Data Correlation Visualizer
+
+Quick streamlit app used to create a scatter plot graph of various survey attributes. Mainly to correlate happiness with other factors. Country list can be found in the happy.csv file.
--- main.py
+++ main.py
@@ -4,44 +4,38 @@ import pandas as pd
 
 
 # pick 3 variables to pull from csv and allow user to select what to plot on x/y axis
+df = pd.read_csv("happy.csv")
 
 st.title("Happiness Data Correlation Visualizer")
-x_option = st.selectbox("Select X-axis to plot: ", ("GDP", "Generosity", "Corruption"))
-y_option = st.selectbox("Select Y-axis to plot: ", ("GDP", "Generosity", "Corruption"))
+x_option = st.selectbox("Select X-axis to plot: ", ("happiness","gdp","social_support","life_expectancy","freedom_to_make_life_choices","generosity","corruption"))
+y_option = st.selectbox("Select Y-axis to plot: ", ("happiness","gdp","social_support","life_expectancy","freedom_to_make_life_choices","generosity","corruption"))
 
-match x_option:
-    case "GDP":
-        x_data = [100,5045,700]
-    case "Generosity":
-        x_data = [80, 150, 300]
-    case "Corruption":
-        x_data = [55, 200, 310]
+x_data = []
+y_data = []
 
-match y_option:
-    case "GDP":
-        y_data = [100,5045,700]
-    case "Generosity":
-        y_data = [80, 150, 300]
-    case "Corruption":
-        y_data = [55, 200, 310]
+def build_x_dataset(x_option):
+    for index, row in df.iterrows():
+        x_data.append(row[x_option])
+    return x_data
+
+def build_y_dataset(y_option):
+    for index, row in df.iterrows():
+        y_data.append(row[y_option])
+    return y_data
+
+x_data = build_x_dataset(x_option)
+y_data = build_y_dataset(y_option)
+
+
+print(f"x_data = {x_data}")
+print(f"y_data = {y_data}")
 
 
 st.subheader(f"{x_option} and {y_option}")
 
 
-figure = px.line(x=x_data, y=y_data, labels={"x": x_option, "y": y_option})
+figure = px.scatter(x=x_data, y=y_data, labels={"x": x_option, "y": y_option})
 st.plotly_chart(figure)
 
 
-# def get_data(days):
-#     #temp data will get from weather data later
-#     dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-#     temperatures = [10,11,15]
-#     temperatures = [days * i for i in temperatures]
-#     return dates, temperatures
-#
-# d, t = get_data(days)
-#
-# figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperatures (C)"})
-# st.plotly_chart(figure)
 
